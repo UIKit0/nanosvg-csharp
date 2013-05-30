f1dcdfb0df8de0664d0f6d6a3479e34aa76525c2
Josh Montoute <support@thinksquirrel.com>
2013-05-30 18:56:34 -0500

    Added support for HTML color names

--- NanoSvg.cs
+++ NanoSvg.cs
@@ -519,11 +519,173 @@ namespace NanoSvg
             }       
         }
         #endregion
-        #region Standard library stuff
-        // This is a fake sscanf
-        static void sscanf(CString s, CString format, ref uint i)
+        #region Color names (static constructor)
+        static System.Collections.Generic.Dictionary<string, string> _colorNames = new System.Collections.Generic.Dictionary<string, string>();
+        
+        static SvgParser()
         {
-            uint.TryParse(s, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture, out i);
+           _colorNames.Add("aliceblue"               , "F0F8FF" );
+           _colorNames.Add("antiquewhite"            , "FAEBD7" );
+           _colorNames.Add("aqua"                    , "00FFFF" );
+           _colorNames.Add("aquamarine"              , "7FFFD4" );
+           _colorNames.Add("azure"                   , "F0FFFF" );
+           _colorNames.Add("beige"                   , "F5F5DC" );
+           _colorNames.Add("bisque"                  , "FFE4C4" );
+           _colorNames.Add("black"                   , "000000" );
+           _colorNames.Add("blanchedalmond"          , "FFEBCD" );
+           _colorNames.Add("blue"                    , "0000FF" );
+           _colorNames.Add("blueviolet"              , "8A2BE2" );
+           _colorNames.Add("brown"                   , "A52A2A" );
+           _colorNames.Add("burlywood"               , "DEB887" );
+           _colorNames.Add("cadetblue"               , "5F9EA0" );
+           _colorNames.Add("chartreuse"              , "7FFF00" );
+           _colorNames.Add("chocolate"               , "D2691E" );
+           _colorNames.Add("coral"                   , "FF7F50" );
+           _colorNames.Add("cornflowerblue"          , "6495ED" );
+           _colorNames.Add("cornsilk"                , "FFF8DC" );
+           _colorNames.Add("crimson"                 , "DC143C" );
+           _colorNames.Add("cyan"                    , "00FFFF" );
+           _colorNames.Add("darkblue"                , "00008B" );
+           _colorNames.Add("darkcyan"                , "008B8B" );
+           _colorNames.Add("darkgoldenrod"           , "B8860B" );
+           _colorNames.Add("darkgray"                , "A9A9A9" );
+           _colorNames.Add("darkgreen"               , "006400" );
+           _colorNames.Add("darkkhaki"               , "BDB76B" );
+           _colorNames.Add("darkmagenta"             , "8B008B" );
+           _colorNames.Add("darkolivegreen"          , "556B2F" );
+           _colorNames.Add("darkorange"              , "FF8C00" );
+           _colorNames.Add("darkorchid"              , "9932CC" );
+           _colorNames.Add("darkred"                 , "8B0000" );
+           _colorNames.Add("darksalmon"              , "E9967A" );
+           _colorNames.Add("darkseagreen"            , "8FBC8F" );
+           _colorNames.Add("darkslateblue"           , "483D8B" );
+           _colorNames.Add("darkslategray"           , "2F4F4F" );
+           _colorNames.Add("darkturquoise"           , "00CED1" );
+           _colorNames.Add("darkviolet"              , "9400D3" );
+           _colorNames.Add("deeppink"                , "FF1493" );
+           _colorNames.Add("deepskyblue"             , "00BFFF" );
+           _colorNames.Add("dimgray"                 , "696969" );
+           _colorNames.Add("dimgrey"                 , "696969" );
+           _colorNames.Add("dodgerblue"              , "1E90FF" );
+           _colorNames.Add("firebrick"               , "B22222" );
+           _colorNames.Add("floralwhite"             , "FFFAF0" );
+           _colorNames.Add("forestgreen"             , "228B22" );
+           _colorNames.Add("fuchsia"                 , "FF00FF" );
+           _colorNames.Add("gainsboro"               , "DCDCDC" );
+           _colorNames.Add("ghostwhite"              , "F8F8FF" );
+           _colorNames.Add("gold"                    , "FFD700" );
+           _colorNames.Add("goldenrod"               , "DAA520" );
+           _colorNames.Add("gray"                    , "808080" );
+           _colorNames.Add("green"                   , "008000" );
+           _colorNames.Add("greenyellow"             , "ADFF2F" );
+           _colorNames.Add("honeydew"                , "F0FFF0" );
+           _colorNames.Add("hotpink"                 , "FF69B4" );
+           _colorNames.Add("indianred"               , "CD5C5C" );
+           _colorNames.Add("indigo"                  , "4B0082" );
+           _colorNames.Add("ivory"                   , "FFFFF0" );
+           _colorNames.Add("khaki"                   , "F0E68C" );
+           _colorNames.Add("lavender"                , "E6E6FA" );
+           _colorNames.Add("lavenderblush"           , "FFF0F5" );
+           _colorNames.Add("lawnGreen"               , "7CFC00" );
+           _colorNames.Add("lemonchiffon"            , "FFFACD" );
+           _colorNames.Add("lightblue"               , "ADD8E6" );
+           _colorNames.Add("lightcoral"              , "F08080" );
+           _colorNames.Add("lightcyan"               , "E0FFFF" );
+           _colorNames.Add("lightgoldenrodyellow"    , "FAFAD2" );
+           _colorNames.Add("lightgray"               , "D3D3D3" );
+           _colorNames.Add("lightgreen"              , "90EE90" );
+           _colorNames.Add("lightpink"               , "FFB6C1" );
+           _colorNames.Add("lightsalmon"             , "FFA07A" );
+           _colorNames.Add("lightseagreen"           , "20B2AA" );
+           _colorNames.Add("lightskyblue"            , "87CEFA" );
+           _colorNames.Add("lightslategray"          , "778899" );
+           _colorNames.Add("lightsteelblue"          , "B0C4DE" );
+           _colorNames.Add("lightyellow"             , "FFFFE0" );
+           _colorNames.Add("lime"                    , "00FF00" );
+           _colorNames.Add("limegreen"               , "32CD32" );
+           _colorNames.Add("linen"                   , "FAF0E6" );
+           _colorNames.Add("magenta"                 , "FF00FF" );
+           _colorNames.Add("maroon"                  , "800000" );
+           _colorNames.Add("mediumaquamarine"        , "66CDAA" );
+           _colorNames.Add("mediumblue"              , "0000CD" );
+           _colorNames.Add("mediumorchid"            , "BA55D3" );
+           _colorNames.Add("mediumpurple"            , "9370DB" );
+           _colorNames.Add("mediumseagreen"          , "3CB371" );
+           _colorNames.Add("mediumslateblue"         , "7B68EE" );
+           _colorNames.Add("mediumspringgreen"       , "00FA9A" );
+           _colorNames.Add("mediumturquoise"         , "48D1CC" );
+           _colorNames.Add("mediumvioletred"         , "C71585" );
+           _colorNames.Add("midnightblue"            , "191970" );
+           _colorNames.Add("mintcream"               , "F5FFFA" );
+           _colorNames.Add("mistyrose"               , "FFE4E1" );
+           _colorNames.Add("moccasin"                , "FFE4B5" );
+           _colorNames.Add("navajowhite"             , "FFDEAD" );
+           _colorNames.Add("navy"                    , "000080" );
+           _colorNames.Add("oldlace"                 , "FDF5E6" );
+           _colorNames.Add("olive"                   , "808000" );
+           _colorNames.Add("olivedrab"               , "6B8E23" );
+           _colorNames.Add("orange"                  , "FFA500" );
+           _colorNames.Add("orangered"               , "FF4500" );
+           _colorNames.Add("orchid"                  , "DA70D6" );
+           _colorNames.Add("palegoldenrod"           , "EEE8AA" );
+           _colorNames.Add("palegreen"               , "98FB98" );
+           _colorNames.Add("paleturquoise"           , "AFEEEE" );
+           _colorNames.Add("palevioletred"           , "DB7093" );
+           _colorNames.Add("papayawhip"              , "FFEFD5" );
+           _colorNames.Add("peachpuff"               , "FFDAB9" );
+           _colorNames.Add("peru"                    , "CD853F" );
+           _colorNames.Add("pink"                    , "FFC0CB" );
+           _colorNames.Add("plum"                    , "DDA0DD" );
+           _colorNames.Add("powderblue"              , "B0E0E6" );
+           _colorNames.Add("purple"                  , "800080" );
+           _colorNames.Add("red"                     , "FF0000" );
+           _colorNames.Add("rosybrown"               , "BC8F8F" );
+           _colorNames.Add("royalblue"               , "4169E1" );
+           _colorNames.Add("saddlebrown"             , "8B4513" );
+           _colorNames.Add("salmon"                  , "FA8072" );
+           _colorNames.Add("sandybrown"              , "F4A460" );
+           _colorNames.Add("seagreen"                , "2E8B57" );
+           _colorNames.Add("seashell"                , "FFF5EE" );
+           _colorNames.Add("sienna"                  , "A0522D" );
+           _colorNames.Add("silver"                  , "C0C0C0" );
+           _colorNames.Add("skyblue"                 , "87CEEB" );
+           _colorNames.Add("slateblue"               , "6A5ACD" );
+           _colorNames.Add("slategray"               , "708090" );
+           _colorNames.Add("snow"                    , "FFFAFA" );
+           _colorNames.Add("springgreen"             , "00FF7F" );
+           _colorNames.Add("steelblue"               , "4682B4" );
+           _colorNames.Add("tan"                     , "D2B48C" );
+           _colorNames.Add("teal"                    , "008080" );
+           _colorNames.Add("thistle"                 , "D8BFD8" );
+           _colorNames.Add("tomato"                  , "FF6347" );
+           _colorNames.Add("turquoise"               , "40E0D0" );
+           _colorNames.Add("violet"                  , "EE82EE" );
+           _colorNames.Add("wheat"                   , "F5DEB3" );
+           _colorNames.Add("white"                   , "FFFFFF" );
+           _colorNames.Add("whitesmoke"              , "F5F5F5" );
+           _colorNames.Add("yellow"                  , "FFFF00" );
+           _colorNames.Add("yellowgreen"             , "9ACD32" );
+        }
+        #endregion
+        #region Standard library stuff
+        // This is a fake sscanf, just to parse colors
+        static void sscanf(CString s, ref uint i)
+        {
+            string str = s.ToString();
+            
+            // 3-digit hex code
+            if (str.Length == 3)
+                str = string.Concat(str[0], str[0], str[1], str[1], str[2], str[2]);
+            
+            // Try to parse
+            if (!uint.TryParse(str, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture, out i))
+            {
+                // Try a color name
+                if (_colorNames.TryGetValue(str.ToLower(System.Globalization.CultureInfo.InvariantCulture), out str))
+                {
+                    uint.TryParse(str, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture, out i);
+                }
+            }
         }
         
         static double atof(CString s)
@@ -973,7 +1135,7 @@ namespace NanoSvg
             while(str.Ref == ' ') ++str;
             if (str.Ref == '#')
             {
-                sscanf(str + 1, "%x", ref c);
+                sscanf(str + 1, ref c);
             }
             return c;
         }
